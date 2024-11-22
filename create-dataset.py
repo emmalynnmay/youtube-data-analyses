4b4b15ec33d46c6603a4859a812f096761e29739
emmalynnnn <emmaruthlynn@gmail.com>
2024-11-21 18:59:52 -0700

    Formatting code to pull video data

--- create-dataset.py
+++ create-dataset.py
@@ -1,27 +1,47 @@
 from googleapiclient.discovery import build
 from dotenv import dotenv_values
+import pandas as pd
 
-config = dotenv_values(".env")
+config = dotenv_values('.env')
 
-def pull_data():
+VIDEO_IDS = ['ZEV7ksLS5dk', 'ZbKd9AuncNI', 'jfWa-GD0szU']
+OUTPUT_NAME = 'output.csv'
 
-    print(config)
+def authenticate():
+    if config['YOUTUBE_API_KEY'] == '':
+        raise Exception('You must add a YouTube API key to the .env file to run this script!')
+    return build('youtube', 'v3', developerKey=config['YOUTUBE_API_KEY'])
 
-    youtube = build('youtube', 'v3', developerKey=config['YOUTUBE_API_KEY'])
-
+def get_video_data(youtube, vid_id):
     request = youtube.videos().list(
-        part="snippet,contentDetails",
-        id="ZEV7ksLS5dk"
+        part='snippet',
+        id=vid_id
     )
     response = request.execute()
+    return response['items'][0]
 
-    title = response['items'][0]['snippet']['title']
-    duration = response['items'][0]['contentDetails']['duration']
-    thumbnail_url = response['items'][0]['snippet']['thumbnails']['default']['url']
+def clean_string_for_csv(string):
+    string = string.strip()
+    string = string.replace('\n', ' ')
+    string = string.replace(',', '\\,')
+    return string
 
-    print("Title: ", title)
-    print("Duration: ", duration)
-    print("Thumbnail URL: ", thumbnail_url)
+def pull_data_for_list(youtube, list, object_type):
+    data = []
+    for item in list:
+        if object_type == 'video':
+            result = get_video_data(youtube, item)
+            to_add = {'id': item}
+            result['snippet']['description'] = clean_string_for_csv(result['snippet']['description'])
+            del result['snippet']['thumbnails']
+            del result['snippet']['localized'] #TODO: do we want to add this back?
+            to_add = {**to_add, **(result['snippet'])}
+            data.append(to_add)
 
-if __name__ == "__main__":
-    pull_data()
+    df = pd.DataFrame(data)
+    return df
+
+if __name__ == '__main__':
+    youtube = authenticate()
+    videos = pull_data_for_list(youtube, VIDEO_IDS, 'video')
+    videos.to_csv(OUTPUT_NAME, index=False)
