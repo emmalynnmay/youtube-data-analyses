5b3aa155bb7dcf2d1495dc8373c8eacb1aa1a955
emmalynnnn <emmaruthlynn@gmail.com>
2024-11-24 15:35:38 -0700

    Fixing pagination, creating dataset

--- create-dataset.py
+++ create-dataset.py
@@ -7,16 +7,15 @@ config = dotenv_values('.env')
 TEST_VIDEO_IDS = ['ZEV7ksLS5dk', 'ZbKd9AuncNI', 'jfWa-GD0szU']
 TEST_PLAYLIST_ID = 'PLCow7UQsPflmxuJnkVv1oEkanC3-ZDvmG'
 PLAYLISTS = {
-    #'USU': TEST_PLAYLIST_ID}
-    'Slam Poetry': 'PL9rT6KUep7uqN8Xioo3R32cUyogH5wVMk'}
-    # 'Minecraft': 'PLKiPmtkaxvOYFJzP847fBEfpr4FZD3tpg',
-    # 'Movie Scenes': 'PLYhREcq3PbZ8duNVstBQ_0jnHC1ffxJfu',
-    # 'Woodworking': 'PLr6O8F0Sz5TrKaAzGdhmmkIoxE-vdRYDE',
-    # 'Space Songs': 'PLG04vLHTI500i3IcAK5Xsgn9F2q66ZOgr',
-    # 'Ancient Memes': 'PLHEBsC_NxJ_R3KnXJa4BfF7NEbnNvW_jg'
-    # }
+    'Slam Poetry': 'PL9rT6KUep7uqN8Xioo3R32cUyogH5wVMk',
+    'Minecraft': 'PLKiPmtkaxvOYFJzP847fBEfpr4FZD3tpg',
+    'Movie Scenes': 'PLYhREcq3PbZ8duNVstBQ_0jnHC1ffxJfu',
+    'Woodworking': 'PLr6O8F0Sz5TrKaAzGdhmmkIoxE-vdRYDE',
+    'Space Songs': 'PLG04vLHTI500i3IcAK5Xsgn9F2q66ZOgr',
+    'Ancient Memes': 'PLHEBsC_NxJ_R3KnXJa4BfF7NEbnNvW_jg'
+    }
 OUTPUT_NAME = 'new-youtube-data.csv'
-PAGE_COUNT_LIMIT = 4#49
+PAGE_COUNT_LIMIT = 50
 
 def authenticate():
     if config['YOUTUBE_API_KEY'] == '':
@@ -28,13 +27,6 @@ def clean_string_for_csv(string):
     string = string.replace('\n', ' ')
     string = string.replace(',', '\\,')
     return string
-
-def format_ids(id_list):
-    ids = ''
-    for the_id in id_list:
-        ids += (the_id + ',')
-    return ids + "hey"
-    return ids[:-1]
 
 def clean_video_data(vid_data, item, genre):
 
@@ -57,60 +49,73 @@ def clean_video_data(vid_data, item, genre):
 
     return to_add
 
-def get_video_data(youtube, vid_ids, vid_ids_formatted, genre):
-    print(vid_ids)
-    print(vid_ids_formatted)
+def get_video_data(youtube, vid_id, genre):
+    print(f"   Pulling data for {vid_id}")
 
     request = youtube.videos().list(
         part='snippet,contentDetails,statistics,paidProductPlacementDetails,topicDetails',
-        id=vid_ids_formatted
+        id=vid_id
     )
-    response = request.execute()
-
-    to_return = []
-    print(f"Length: {len(vid_ids)}")
-    for vid_index in range(len(vid_ids)):
-
-        if vid_index >= len(vid_ids):
-            print("Huh that's weird")
-            continue
-
-        print(vid_index)
-        to_return.append(clean_video_data(response['items'][vid_index], vid_ids[vid_index], genre))
     
-    return to_return
+    response = request.execute()
+    if len(response['items']) == 0:
+        print("      Video not available :(")
+        return None
+
+    return clean_video_data(response['items'][0], vid_id, genre)
 
 def pull_data_for_list(youtube, list, object_type, genre):
     data = []
-    index = 0
-    while True:
-        print(f"   Getting videos {index} to {index + PAGE_COUNT_LIMIT - 1}")
-        this_batch_of_ids = format_ids(list[index:index + PAGE_COUNT_LIMIT])
+    for item in list:
         if object_type == 'video':
-            result = get_video_data(youtube, list[index:index + PAGE_COUNT_LIMIT], this_batch_of_ids, genre)
-            data += result
-        else:
-            raise Exception(f"Object type {object_type} not recognized.")
-        
-        if index + PAGE_COUNT_LIMIT <= len(list) - 1:
-            index += (PAGE_COUNT_LIMIT - 1)
-        else:
-            break
+            try:
+                result = get_video_data(youtube, item, genre)
+                if result != None:
+                    data.append(result)
+            except:
+                continue
 
     df = pd.DataFrame(data)
     return df
+
+def check_if_exists(response, key):
+    try:
+        result = response[key]
+        return True
+    except:
+        return False
 
 def get_ids_in_playlist(youtube, playlist_id):
     to_return = []
 
     request = youtube.playlistItems().list(
         part='contentDetails',
-        playlistId=playlist_id
+        playlistId=playlist_id,
+        maxResults=PAGE_COUNT_LIMIT,
     )
     response = request.execute()
     
     for vid in response['items']:
         to_return.append(vid['contentDetails']['videoId'])
+
+    if check_if_exists(response, 'nextPageToken'):
+        next_page_token = response['nextPageToken']
+
+        while True:
+            request = youtube.playlistItems().list(
+                part='contentDetails',
+                playlistId=playlist_id,
+                maxResults=PAGE_COUNT_LIMIT,
+                pageToken=next_page_token
+            )
+            response = request.execute()
+            for vid in response['items']:
+                to_return.append(vid['contentDetails']['videoId'])
+
+            if check_if_exists(response, 'nextPageToken'):
+                next_page_token = response['nextPageToken']
+            else:
+                break
 
     return to_return
 
@@ -120,7 +125,7 @@ def create_dataset(youtube):
     for playlist_name, playlist_id in PLAYLISTS.items():
         print(f"Pulling video data for playlist {playlist_name}...")
         ids_in_playlist = get_ids_in_playlist(youtube, playlist_id)
-        print(f"All the ids: {ids_in_playlist}")
+        print(f"   Found {len(ids_in_playlist)} video ids...")
         videos = pull_data_for_list(youtube, ids_in_playlist, 'video', playlist_name)
         video_collections.append(videos)
 
